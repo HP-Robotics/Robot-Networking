776582388fa115405f2a864f7a105182e8d8b8d1
Marcell Toth <tothmarcell97@gmail.com>
2014-01-30 15:13:13 -0600

    networbuffer_out finished

--- networkbuffer_out.cpp
+++ networkbuffer_out.cpp
@@ -2,11 +2,13 @@
 
 #include <cstring>
 #include <cstdlib>
+#include <string>
+#include <arpa/inet.h>
 
 networkbuffer_out::networkbuffer_out(const unsigned char* raw, int length)
 	:_buffer(0), _size(length), _offset(0)
 {
-	_buffer = malloc(length * sizeof(char));
+	_buffer = (unsigned char*)malloc(length * sizeof(char));
 	memcpy(_buffer, raw, length);
 }
 
@@ -34,35 +36,52 @@ networkbuffer_out& networkbuffer_out::operator>>(int& v)
 }
 networkbuffer_out& networkbuffer_out::operator>>(unsigned char& v)
 {
-	v = v[_offset++];
+	v = _buffer[_offset++];
+	return *this;
 }
 networkbuffer_out& networkbuffer_out::operator>>(unsigned short& v)
 {
 	unsigned char big, small;
-	big = v[_offset++];
-	small = v[_offset++];
+	big = _buffer[_offset++];
+	small = _buffer[_offset++];
 	unsigned short network = (((short)big) << 8) | small;
 	
 	v = ntohs(network);
+	return *this;
 }
 networkbuffer_out& networkbuffer_out::operator>>(unsigned int& v)
 {
 	//sorry about these variable names :D
 	unsigned char bigest, big, small, smallest;
-	bigest = v[_offset++];
-	big = v[_offset++];
-	small = v[_offset++];
-	smallest = v[_offset++];
-	unsigned int network = (((int)bigest) << 24) | ((int)big) << 16) | ((int)big) << 8) | small;
+	bigest = _buffer[_offset++];
+	big = _buffer[_offset++];
+	small = _buffer[_offset++];
+	smallest = _buffer[_offset++];
+	unsigned int network = (((int)bigest) << 24) | (((int)big) << 16) | (((int)small) << 8) | smallest;
 	
 	v = ntohs(network);
+	return *this;
 }
 networkbuffer_out& networkbuffer_out::operator>>(string& v)
 {
-	
+	v = string((const char*)_buffer + _offset);
+	_offset += strlen((const char*)_buffer + _offset);
+	return *this;
 }
-int copyfrom(char* buffer, int bytestocopy)
+int networkbuffer_out::copyfrom(char* buffer, int bytestocopy)
 {
-	//TODO:
-	//memcpy(buffer, (_buffer + _offset), bytestocopy) 
+	int remaining = _size - _offset;
+	if(remaining >= bytestocopy)
+	{
+		memcpy(buffer, (_buffer + _offset), bytestocopy);
+		_offset += bytestocopy;
+		return bytestocopy;
+	}
+	else
+	{
+		memcpy(buffer, (_buffer + _offset), remaining);
+		_offset += remaining;
+		return remaining;
+	}
+	
 }
